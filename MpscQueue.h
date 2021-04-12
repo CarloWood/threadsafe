9c9c7b8a1f0151623e6bce4de854dc6952cc6632
Carlo Wood <carlo@alinoe.com>
2021-04-12 11:35:00 +0200

    Add more analysis and memory orders.

--- MpscQueue.h
+++ MpscQueue.h
@@ -31,6 +31,9 @@
 // the authors and should not be interpreted as representing official policies,
 // either expressed or implied, of Dmitry Vyukov.
 //
+//
+// All comments and memory orders used are Copyright (c) 2021 Carlo Wood,
+// and also released under the same license.
 
 #include <atomic>
 
@@ -106,6 +109,84 @@ struct MpscNode
 // encountered). Except for an empty list, because there has to be always at
 // least one node in the list; so in that case m_stub is always there.
 //
+//
+// Memory Order
+// ------------
+//
+// Sequential-consistent ordering isn't needed: the producers, that call push,
+// only store (to m_next); they load the value of m_head but that value is
+// only written to by the std::atomic_exchange at the same time. This Read-
+// Modify-Write operation has a total ordering anyway.
+//
+// The consumer, that calls pop, is the only thread (or multiple threads
+// where a total ordering is already enforced and only one at a time calls
+// pop) so there is no modification order that could be different between
+// two or more threads anyway.
+//
+// In other words, the std::atomic_exchange in push() has a total ordering
+// and determines the order in which nodes are pushed onto the queue.
+// The consumer will pop nodes in that order.
+//
+// At the time of writing, release-consume ordering is still is still being
+// discouraged as its specification is being revised (since C++17), so that
+// leaves release-acquire ordering.
+//
+// Whenever a pointer to a node is returned by pop, the data that was written
+// to that node must be visible (you'd think that this is typical a release-consume
+// case; but at least on Intel architectures it shouldn't make any difference).
+// The returned value is tail, which is a local variable assigned from m_tail
+// or next, where also m_tail was assigned from next, where next is loaded
+// from m_next. In other words, we have to read m_next with memory order acquire,
+// and the non-null store in push with memory order release.
+//
+// In pop() this only leaves the load of m_head.
+// At first sight, it seems pointless to read m_head at all: immediately
+// after loading it it could be changed again by any number of calls to push
+// by other threads. However, after any call to push, m_head would at most
+// be changed away from the value of tail, making the expression tail != head
+// true. So, in terms of FuzzyBool (see utils/FuzzyBool.h) the construct:
+//
+//  MpscNode* head = m_head.load();
+//  utils::FuzzyBool unequal = (tail != head) ? fuzzy::True : fuzzy::WasFalse;
+//  if (unequal.is_true())
+//    return nullptr;
+//
+// is appropriate.
+//
+// This means that the subsequent call to push(&m_stub) guarantees that
+// `unequal` becomes (and stays) True, so that if the function subsequently
+// returns nullptr and reenters, it will never call push(&m_stub) again.
+// In turn this then guarantees that there is only one m_stub in the linked
+// list at all times, which is important because it only has one m_next
+// pointer, of course.
+//
+// The release-acquire ordering of m_next already guarantees that the
+// value of m_head that is loaded isn't some insanely old value pointing
+// to ages old values: it HAS to be the value written by the atomic_exchange
+// just prior to the value that we loaded from m_next at the top of pop(),
+// or any later value of course. It can not be an older value because that
+// atomic_exchange store is visible, and RMW's have a total order, so the
+// last value written will hide any previously written values.
+//
+// The call to pop() at this point could return nullptr and reenter at
+// the top, get to the same point and load m_head again; this value is
+// now guaranteed to be unequal to the last value of m_head, and therefore
+// unequal to m_tail, because it is the very same thread that just stored
+// a value to m_head (by calling push(&m_stub).
+//
+// It is therefore perfectly fine to load the value of m_head with memory
+// order relaxed.
+//
+// The store of the nullptr in push() can also be memory order relaxed,
+// because we don't use anything that was written to memory before that
+// point. In most cases, when the value of nullptr is read, pop() just
+// returns nullptr. In the one case that it doesn't, it pushes m_stub
+// a more or less starts over.
+//
+// Finally, the atomic_exchange can be relaxed too, since it is ordered
+// with respect to itself anyway, and the only other instance where we
+// read it it is loaded with memory order relaxed.
+//
 class MpscQueue
 {
  private:
@@ -118,18 +199,18 @@ class MpscQueue
 
   void push(MpscNode* node)
   {
-    node->m_next.store(nullptr);
-    MpscNode* prev = std::atomic_exchange(&m_head, node);
+    node->m_next.store(nullptr, std::memory_order_relaxed);
+    MpscNode* prev = m_head.exchange(node, std::memory_order_relaxed);
     // Here m_head points to the new node, which either points to null
     // or already points to the NEXT node that was pushed AND completed, etc.
     // Now fix the next pointer of the node that m_head was pointing at.
-    prev->m_next.store(node);
+    prev->m_next.store(node, std::memory_order_release);
   }
 
   MpscNode* pop()
   {
     MpscNode* tail = m_tail;
-    MpscNode* next = tail->m_next.load();
+    MpscNode* next = tail->m_next.load(std::memory_order_acquire);
     if (tail == &m_stub)
     {
       // If m_tail ---> m_stub ===> nullptr, at the time of the above load(), then return nullptr.
@@ -141,7 +222,7 @@ class MpscQueue
       // Skip m_stub.
       m_tail = next;
       tail = next;
-      next = next->m_next.load();
+      next = next->m_next.load(std::memory_order_acquire);
     }
     // Are there at least two nodes in the list?
     // Aka, m_tail ---> node1 ===> node2
@@ -154,9 +235,10 @@ class MpscQueue
     // If we get here we had the situation, at the time of the above load(), of
     // m_tail ---> [m_stub ===>] node1 ===> nullptr and we now have
     // tail ---> node1; where, at least very recently, node1 ===> nullptr.
-    MpscNode* head = m_head.load();
+    MpscNode* head = m_head.load(std::memory_order_relaxed);
     // If head was changed in the meantime then a push is or was in progress
-    // and we fail to read node1 for now.
+    // and we fail to read node1 for now. This is to make sure we won't push
+    // m_stub onto the list when it is already there.
     if (tail != head)
       return nullptr;
     // Make sure we have at least two nodes again.
@@ -177,7 +259,7 @@ class MpscQueue
     //                                                                 m_head
     // where node2 was (incompletely) pushed before we pushed &m_stub.
     // In that case this next will be null:
-    next = tail->m_next.load();
+    next = tail->m_next.load(std::memory_order_acquire);
     if (next)
     {
       // Remove node and return it.
