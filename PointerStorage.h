e378d62904350cb10f657978227c873c54f38ab2
Carlo Wood <carlo@alinoe.com>
2022-06-02 23:36:49 +0200

    Update PGP finger print.

--- PointerStorage.h
+++ PointerStorage.h
@@ -1,3 +1,30 @@
+/**
+ * threadsafe -- Threading utilities: object oriented (read/write) locking and more.
+ *
+ * @file
+ * @brief Declaration of class PointerStorage.
+ *
+ * @Copyright (C) 2022  Carlo Wood.
+ *
+ * pub   dsa3072/C155A4EEE4E527A2 2018-08-16 Carlo Wood (CarloWood on Libera) <carlo@alinoe.com>
+ * fingerprint: 8020 B266 6305 EE2F D53E  6827 C155 A4EE E4E5 27A2
+ *
+ * This file is part of threadsafe.
+ *
+ * Threadsafe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Threadsafe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with threadsafe.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
 #include "AIReadWriteSpinLock.h"
